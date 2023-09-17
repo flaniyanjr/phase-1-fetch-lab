143a2c6f60648fdb2e2e9144fde4376e11477ffc
Felix Laniyan <flaniyanjr@gmail.com>
2023-09-17 18:35:25 -0400

    ready to submit

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  fetch('https://anapioficeandfire.com/api/books')
+  .then((response) => {
+    return response.json()
+  })
+  .then(((data) => {
+    renderBooks(data)
+  }))
   
 }
 
